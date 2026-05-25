a88cc3c89a26e42fa54113ea4556d75b359cefc6
Fooxii <ironata07@gmail.com>
2026-05-25 14:54:06 -0400

    feat: add teachClass function to handle subject teaching logic

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -52,3 +52,12 @@ function executeWork(employee: Director | Teacher): string {
     }
     return employee.workTeacherTasks();
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    return 'Teaching History';
+}
